16ecdc6e3f256239ead5d2f84b3f76552385acb9
chionz <chideraprince9@gmail.com>
2025-01-29 19:11:19 +0100

    changed datetime format

--- hngapi/views.py
+++ hngapi/views.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timezone
 from django.shortcuts import render
 
 from django.http import JsonResponse
@@ -7,7 +7,7 @@ from django.views.decorators.http import require_GET
 @require_GET
 def stage0(request):
     email = "chideraprince9@gmail.com"
-    date = datetime.datetime.now().isoformat()
+    date = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
     github = "https://github.com/chionz/HNGStage0"
 
     response ={
